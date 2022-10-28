5e13e89beae0a9eabee8846b7c6030f0d61ad9fd
David Craven <david@craven.ch>
2022-10-28 12:49:40 +0200

    Add Dockerfile. (#1)
    * Add Dockerfile.
    
    * Fix range function.

--- src/services/Utils.ts
+++ src/services/Utils.ts
@@ -18,10 +18,10 @@ import rundef from 'rundef';
 
 export class Utils {
     static range = (min: number, max: number): number[] => {
-        const len = max - min - 1;
+        const len = max - min + 1;
         const arr = new Array(len);
-        for (let i = 0; i <= len; i++) {
-            arr[i] = min + 1 + i;
+        for (let i = 0; i < len; i++) {
+            arr[i] = min + i;
         }
         return arr;
     };
